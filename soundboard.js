c5a7343cf913d31646a8fbe857c469c9f4f218df
Grant Sheridan <sherid90@msu.edu>
2021-06-19 14:40:49 -0400

    added sound vars

---
+++ soundboard.js
@@ -0,0 +1,8 @@
+var horn = new Audio ();
+horn.src = "car_horn_x.wav";
+
+var cash = new Audio ();
+cash.src = "cash_register_x.wav";
+
+var clock = new Audio ();
+clock.src = "cuckoo_clock2_x.wav";
